5d8769f7a7fc922dff1ca3278a52c0f31932bab5
Carlos Eduardo <schuster@bol.com.br>
2020-02-11 09:30:42 -0300

    Update conexao1.php

--- conexao1.php
+++ conexao1.php
@@ -4,19 +4,19 @@ header('Content-Type: text/html; charset=utf-8');
 //ini_set('default_charset','UTF-8');
 
 $hostname_conexao1 = "localhost";
-$database_conexao1 = "sa_consultoria";
+$database_conexao1 = "nome_banco";
 $username_conexao1 = "root";
-$password_conexao1 = "";
+$password_conexao1 = "xyz";
 
-$conexao1 = mysqli_connect($hostname_conexao1, $username_conexao1, $password_conexao1);
-if (!$conexao1)
+$con1 = mysqli_connect($hostname_conexao1, $username_conexao1, $password_conexao1);
+if (!$con1)
   {
   // Handle error - notify administrator, log to a file, show an error screen, etc.
   die("Connection error: " . mysqli_connect_error());
   }
   
-mysqli_query($conexao1, "SET NAMES 'utf8'");
-mysqli_query($conexao1, 'SET character_set_connection=utf8');
-mysqli_query($conexao1, 'SET character_set_client=utf8');
-mysqli_query($conexao1, 'SET character_set_results=utf8');
+mysqli_query($con1, "SET NAMES 'utf8'");
+mysqli_query($con1, 'SET character_set_connection=utf8');
+mysqli_query($con1, 'SET character_set_client=utf8');
+mysqli_query($con1, 'SET character_set_results=utf8');
 ?>
